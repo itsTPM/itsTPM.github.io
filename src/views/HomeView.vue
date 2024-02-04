26ffdf9b5e0561add3d0890109005397b3151169
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-05 00:07:55 +0300

    feat: started making a card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <script setup></script>
 
-<template></template>
-
-<style scoped></style>
+<template>
+  <div class="h-[600px] w-[1000px] rounded-lg bg-amber-300">
+    <section class="flex h-full w-[256px] flex-col rounded-s-lg bg-red-400">
+      <div class="h-[256px] w-[256px] rounded-ss-lg bg-blue-400">
+        <!-- Avatar -->
+      </div>
+      <div class="flex flex-grow flex-col items-center justify-between rounded-es-lg bg-green-400 p-6">
+        <div class="flex flex-col items-center justify-center">
+          <span class="font-mono text-[40px] leading-10">itsTPM</span>
+          <span class="font-ubuntu text-[16px] font-light">pseudo-frontend™</span>
+        </div>
+        <div class="grid grid-cols-3 grid-rows-1 gap-4">
+          <div class="h-[58px] w-[58px] rounded-lg bg-pink-400"></div>
+          <div class="h-[58px] w-[58px] rounded-lg bg-pink-400"></div>
+          <div class="h-[58px] w-[58px] rounded-lg bg-pink-400"></div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
