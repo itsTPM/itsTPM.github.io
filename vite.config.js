d43a333a034439e58085f5bf212e66eab11e3967
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-04 22:48:34 +0300

    refactor: auto format & cleanup (prettier)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import tailwind from "tailwindcss";
-import autoprefixer from "autoprefixer";
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
