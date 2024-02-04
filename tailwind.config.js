7f795aa6d61b33503156284f98e7abb8aeacec88
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-04 23:56:33 +0300

    css: add custom 'mono' and 'ubuntu' font families in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,10 @@ module.exports = {
         'collapsible-up': 'collapsible-up 0.2s ease-in-out',
       },
     },
+    fontFamily: {
+      mono: ['JetBrains Mono', 'monospace'],
+      ubuntu: ['Ubuntu', 'sans-serif'],
+    },
   },
   plugins: [animate],
 };
