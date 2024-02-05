38c5d561646f6f7e46e51a9b74ee52963d8ccda0
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-05 15:38:18 +0300

    feat: add openLink func to sidebar buttons

--- src/components/Card/CardSidebarButton.vue
+++ src/components/Card/CardSidebarButton.vue
@@ -7,11 +7,16 @@ const props = defineProps({
 });
 
 const iconSrc = new URL(`/src/assets/icons/${props.item.icon}`, import.meta.url).href;
+
+const openLink = () => {
+  window.open(props.item.url);
+};
 </script>
 
 <template>
   <button
-    class="flex h-[58px] w-[58px] items-center justify-center rounded-lg border bg-[rgba(0,0,0,0.2)] transition hover:scale-110">
+    class="flex h-[58px] w-[58px] items-center justify-center rounded-lg border bg-[rgba(0,0,0,0.2)] transition hover:scale-110"
+    @click="openLink">
     <img :alt="item.title" :src="iconSrc" class="h-[32px] w-[32px] select-none" />
   </button>
 </template>
