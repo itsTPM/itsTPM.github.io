3a78c49a5b5ecebccb6c03e8075298648529a187
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-05 14:41:48 +0300

    css: add select-none class to buttons & icons

--- src/components/Card/CardContent.vue
+++ src/components/Card/CardContent.vue
@@ -61,13 +61,13 @@ const lists = [
         <div
           v-for="(item, idx) in list.items"
           :key="idx"
-          class="flex flex-col items-center justify-center rounded-lg border bg-[rgba(0,0,0,0.2)] p-3 transition hover:scale-110">
+          class="flex select-none flex-col items-center justify-center rounded-lg border bg-[rgba(0,0,0,0.2)] p-3 transition hover:scale-110">
           <img :src="item.icon" alt="" class="h-[64px] w-[64px] object-contain" />
           <span>{{ item.name }}</span>
         </div>
       </div>
     </div>
 
-    <span class="text-[32px]">My Projects -></span>
+    <span class="select-none text-[32px]">My Projects -></span>
   </section>
 </template>
