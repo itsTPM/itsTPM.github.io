4fc2b3faa526c52d423415ca19f90f87ad6de13f
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-05 14:15:31 +0300

    feat: add card content component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,11 @@
 <script setup>
 import CardSidebar from '@/components/Card/CardSidebar.vue';
+import CardContent from '@/components/Card/CardContent.vue';
 </script>
 
 <template>
-  <div class="h-[600px] w-[1000px] rounded-lg bg-amber-300">
+  <div class="flex h-[600px] w-[1000px] rounded-lg bg-amber-300">
     <CardSidebar></CardSidebar>
+    <CardContent></CardContent>
   </div>
 </template>
---
+++ src/components/Card/CardContent.vue
@@ -0,0 +1,73 @@
+<script setup>
+const lists = [
+  {
+    name: 'Tech Stack',
+    items: [
+      {
+        name: 'Vue.js',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
+      },
+      {
+        name: 'React.js',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      },
+      {
+        name: 'Node.js',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+      },
+      {
+        name: 'Express.js',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
+      },
+      {
+        name: 'MongoDB',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg',
+      },
+    ],
+  },
+  {
+    name: "Tools I'm using",
+    items: [
+      {
+        name: 'VS Code',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg',
+      },
+      {
+        name: 'Figma',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg',
+      },
+      {
+        name: 'Git',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg',
+      },
+      {
+        name: 'GitHub',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg',
+      },
+      {
+        name: 'Vite',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg',
+      },
+    ],
+  },
+];
+</script>
+
+<template>
+  <section class="flex h-full w-full flex-col justify-center gap-9 rounded-r-lg bg-purple-300 p-10">
+    <div v-for="(list, idx) in lists" :key="idx" class="flex flex-col gap-9">
+      <span class="text-2xl">{{ list.name }}</span>
+      <div class="grid grid-cols-5 grid-rows-1 gap-[20px]">
+        <div
+          v-for="(item, idx) in list.items"
+          :key="idx"
+          class="flex flex-col items-center justify-center rounded-lg border border-[#222] bg-orange-400 p-3">
+          <img :src="item.icon" alt="" class="h-[64px] w-[64px] object-contain" />
+          <span>{{ item.name }}</span>
+        </div>
+      </div>
+    </div>
+
+    <span class="text-[32px]">My Projects -></span>
+  </section>
+</template>
