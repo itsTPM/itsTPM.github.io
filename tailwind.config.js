3f9769b8d565cd0a53f4e43c6799949c3b921817
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-05 15:45:33 +0300

    feat: add bg for card

--- tailwind.config.js
+++ tailwind.config.js
@@ -85,6 +85,9 @@ module.exports = {
         'collapsible-down': 'collapsible-down 0.2s ease-in-out',
         'collapsible-up': 'collapsible-up 0.2s ease-in-out',
       },
+      backgroundImage: {
+        'card-avatar': "url('@/assets/bg.png')",
+      },
     },
     fontFamily: {
       sans: ['Inter', 'Open Sans', 'sans-serif'],
