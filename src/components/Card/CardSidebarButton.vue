26bc5577af169e60db47f39b643b38f5c619b4af
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-06 19:08:35 +0300

    feat: change sidebarButton tag from button to a & set draggable: false

--- src/components/Card/CardSidebarButton.vue
+++ src/components/Card/CardSidebarButton.vue
@@ -7,16 +7,13 @@ const props = defineProps({
 });
 
 const iconSrc = new URL(`/src/assets/icons/${props.item.icon}`, import.meta.url).href;
-
-const openLink = () => {
-  window.open(props.item.url);
-};
 </script>
 
 <template>
-  <button
+  <a
+    :href="item.url"
     class="flex h-[58px] w-[58px] items-center justify-center rounded-lg border bg-[rgba(0,0,0,0.2)] transition hover:scale-110"
-    @click="openLink">
-    <img :alt="item.title" :src="iconSrc" class="h-[32px] w-[32px] select-none" />
-  </button>
+    target="_blank">
+    <img :alt="item.title" :src="iconSrc" class="h-[32px] w-[32px] select-none" draggable="false" />
+  </a>
 </template>
