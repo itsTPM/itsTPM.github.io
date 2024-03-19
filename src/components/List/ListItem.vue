fe00105a5fa351121152f5db22f10d2acae919b3
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-03-20 00:32:29 +0300

    feat(ListItem): add alt text to image element
    accessibility improvement

--- src/components/List/ListItem.vue
+++ src/components/List/ListItem.vue
@@ -12,7 +12,7 @@ const iconSrc = new URL(`/src/assets/icons/${props.item.icon}`, import.meta.url)
 <template>
   <div
     class="flex select-none flex-col items-center justify-center rounded-lg border bg-black/20 p-3 transition motion-safe:hover:scale-110">
-    <img :src="iconSrc" alt="" class="h-16 w-16 object-contain" draggable="false" />
+    <img :src="iconSrc" class="h-16 w-16 object-contain" draggable="false" :alt="`${item.name} Logo`" />
     <span>{{ item.name }}</span>
   </div>
 </template>
